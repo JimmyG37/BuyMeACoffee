24785f46d9cc331e29ef9d91224e7e697a7c4223
Jimmy Garcia <Jgarcia3000@gmail.com>
2022-07-27 15:19:17 -0700

    method to buy contract owner a coffee

--- contracts/BuyMeACoffee.sol
+++ contracts/BuyMeACoffee.sol
@@ -1,4 +1,45 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-contract BuyMeACoffee {}
+error BuyMeACoffee__PriceMustBeAboveZero();
+
+contract BuyMeACoffee {
+    struct Memo {
+        address from;
+        uint256 timestamp;
+        string name;
+        string message;
+    }
+
+    address payable i_owner;
+    Memo[] s_memos;
+
+    event NewMemo(
+        address indexed from,
+        uint256 timestamp,
+        string name,
+        string message
+    );
+
+    constructor() {
+        i_owner = payable(msg.sender);
+    }
+
+    /**
+     * @dev buy a coffee for contract owner
+     * @param _name name of the coffee buyer
+     * @param _message a nice message from the coffee buyer
+     */
+    function buyCoffee(string memory _name, string memory _message)
+        public
+        payable
+    {
+        if (msg.value <= 0) {
+            revert BuyMeACoffee__PriceMustBeAboveZero();
+        }
+
+        s_memos.push(Memo(msg.sender, block.timestamp, _name, _message));
+
+        emit NewMemo(msg.sender, block.timestamp, _name, _message);
+    }
+}
